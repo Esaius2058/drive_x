750c3450baf70e0752c8e7e84ba28820c675bf0b
Esaius2058 <wabwirewild@gmail.com>
2025-02-19 22:21:58 +0300

    formatted code using prettier

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import * as dotenv from "dotenv";
-import express, {Application} from "express";
+import express, { Application } from "express";
 import session from "express-session";
-import {PrismaSessionStore} from '@quixo3/prisma-session-store';
-import {PrismaClient} from "@prisma/client";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
 import passport from "passport";
 import path from "path";
 dotenv.config();
@@ -15,25 +15,24 @@ app.set("views", path.resolve(__dirname, "../src", "views"));
 // Middleware to parse incoming JSON requests into JavaScript objects
 app.use(express.json());
 // Middleware to parse URL-encoded data (like form submissions)
-app.use(express.urlencoded({extended: true}));
-app.use(session({
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
     cookie: {
-        maxAge: 7 * 24 * 60 * 60 * 1000 //ms
+      maxAge: 7 * 24 * 60 * 60 * 1000, //ms
     },
     secret: "a santa at nasa",
     resave: true,
     saveUninitialized: true,
-    store: new PrismaSessionStore(
-        new PrismaClient(),
-        {
-            checkPeriod: 2 * 60 * 1000, //ms
-            dbRecordIdIsSessionId: true, //A flag indicating to use the session ID as the Prisma Record ID
-            dbRecordIdFunction: undefined, //A function to generate the Prisma Record ID for a given session ID
-        }
-    )
-}));
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true, //A flag indicating to use the session ID as the Prisma Record ID
+      dbRecordIdFunction: undefined, //A function to generate the Prisma Record ID for a given session ID
+    }),
+  })
+);
 //Middleware that initializes Passport.
-app.use(passport.initialize()); 
+app.use(passport.initialize());
 //Middleware that integrates Passport with Express sessions.
 app.use(passport.session());
 // Directory for static files
