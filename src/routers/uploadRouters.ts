60338270472fdeb3cc081903ea8aed609983761a
Esaius2058 <wabwirewild@gmail.com>
2025-02-21 09:55:49 +0300

    add route for signing up new users

--- src/routers/uploadRouters.ts
+++ src/routers/uploadRouters.ts
@@ -1,30 +1,45 @@
-import {Router, Response, Request} from "express";
+import { Router, Response, Request } from "express";
 import multer from "multer";
-import { uploadSingleFile, uploadMultipleFiles, ensureAuthenticated} from "../controllers/uploadController";
+import {
+  uploadSingleFile,
+  uploadMultipleFiles,
+  ensureAuthenticated,
+  createUser
+} from "../controllers/uploadController";
 
 const router = Router();
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => cb(null, "uploads/"),
-    filename: (req, file, cb) => {
-        const fileName = req.body.filename || file.originalname;
-        cb(null, `${Date.now()}-${fileName}`);
-    }, 
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) => {
+    const fileName = req.body.filename || file.originalname;
+    cb(null, `${Date.now()}-${fileName}`);
+  },
 });
 
 const upload = multer({ storage });
 
 router.get("/", (req: Request, res: Response) => {
-    res.render("welcome", {title: "DriveX"});
+  res.render("welcome", { title: "DriveX" });
 });
 router.get("/log-in", (req: Request, res: Response) => {
-    res.render("log-in", {title: "Login Form"});
+  res.render("log-in", { title: "Login Form" });
+});
+router.get("/sign-up", (req: Request, res: Response) => {
+    res.render("sign-up", { title: "DriveX SignUp"});
 });
 router.get("/profile", ensureAuthenticated, (req: Request, res: Response) => {
-    res.render("profile", {title: "Profile"});
+  res.render("profile", { title: "Profile" });
 });
-router.get("/upload", (req: Request, res: Response) => {
-    res.render("upload-form", {title: "Upload Form"});
+router.get("/upload", ensureAuthenticated, (req: Request, res: Response) => {
+  res.render("upload-form", { title: "Upload Form" });
 });
+router.post("/sign-up", createUser);
 router.post("/upload", upload.single("file-upload"), uploadSingleFile);
-router.post("/upload-multiple", upload.array("files-upload"), uploadMultipleFiles);
+router.post(
+  "/upload-multiple",
+  upload.array("files-upload"),
+  uploadMultipleFiles
+);
+
+export default router;
