b201b7a0f9c9c67c8bffdb37236fe6b20340597a
Esaius2058 <wabwirewild@gmail.com>
2025-04-04 12:10:26 +0300

    removed code for implementing express sessions and passport auth

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,5 @@
 import * as dotenv from "dotenv";
 import express, { Application } from "express";
-import session from "express-session";
-import { PrismaSessionStore } from "@quixo3/prisma-session-store";
-import { PrismaClient } from "@prisma/client";
-import passport from "passport";
 import router from "./routers/uploadRouters";
 import path from "path";
 dotenv.config();
@@ -17,29 +13,10 @@ app.set("views", path.resolve(__dirname, "../src", "views"));
 app.use(express.json());
 // Middleware to parse URL-encoded data (like form submissions)
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    cookie: {
-      maxAge: 7 * 24 * 60 * 60 * 1000, //ms
-    },
-    secret: "a santa at nasa",
-    resave: true,
-    saveUninitialized: true,
-    store: new PrismaSessionStore(new PrismaClient(), {
-      checkPeriod: 2 * 60 * 1000, //ms
-      dbRecordIdIsSessionId: true, //A flag indicating to use the session ID as the Prisma Record ID
-      dbRecordIdFunction: undefined, //A function to generate the Prisma Record ID for a given session ID
-    }),
-  })
-);
-//Middleware that initializes Passport.
-app.use(passport.initialize());
-//Middleware that integrates Passport with Express sessions.
-app.use(passport.session());
 //Initialize the router directory
 app.use("/", router);
 // Directory for static files
 app.use(express.static(path.join(__dirname, "public")));
 
-const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 5000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
