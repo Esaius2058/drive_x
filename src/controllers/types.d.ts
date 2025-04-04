cbf8fa9607de55476db6e6a07e129ceb2d6966dd
Esaius2058 <wabwirewild@gmail.com>
2025-04-04 12:10:26 +0300

    defined a CustomRequest which contains tokens

--- src/controllers/types.d.ts
+++ src/controllers/types.d.ts
@@ -1,7 +1,9 @@
 import { CustomUser} from "../services/userService";
+import {CustomRequest} from "../controllers/uploadController"
 
 declare global {
   namespace Express {
     interface User extends CustomUser{}
+    interface Request extends CustomRequest {}
   }
 }
