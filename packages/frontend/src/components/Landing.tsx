8d131cc6a93a2fea402d21e7b5d8884900684241
Esaius2058 <wabwirewild@gmail.com>
2025-05-04 21:24:38 +0300

    formatted the file using prettier

--- packages/frontend/src/components/Landing.tsx
+++ packages/frontend/src/components/Landing.tsx
@@ -3,63 +3,63 @@ import { LandingNavBar } from "./NavBar";
 import Footer from "./Footer";
 
 const Landing = () => {
-    return (
-        <div className="landing-page">
-            <LandingNavBar />
-            <section className="landing-hero" id="landing-hero">
-                <div className="hero-content">
-                    <div className="hero-tagline">
-                        <h1>Drive X</h1>
-                        <h2>Securely upload and access your files from anywhere.</h2>
-                        <div className="button-div">
-                            <button className="primary-btn">
-                                <Link to="/auth/signup">Get Started</Link>
-                            </button>
-                            <button className="tertiary-btn">Learn More</button>
-                        </div>
-                    </div>
-                </div>
-                <img src="" alt="drive-x" />
-            </section>
-            <section className="landing-features" id="landing-features">
-                <div className="feature">
-                    <img src="" alt="encryption" />
-                    <div className="feature-description">
-                        <h1>End to End Encryption</h1>
-                        <button className="tertiary-btn">Read More</button>
-                    </div>
-                </div>
-                <div className="feature">
-                    <img src="" alt="uploads" />
-                    <div className="feature-description">
-                        <h1>Fast Uploads</h1>
-                        <button className="tertiary-btn">Read More</button>
-                    </div>
-                </div>
-                <div className="feature">
-                    <img src="" alt="folders" />
-                    <div className="feature-description">
-                        <h1>Smart Folders</h1>
-                        <button className="tertiary-btn">Read More</button>
-                    </div>
-                </div>
-            </section>
-            <section className="landing-pivot" id="landing-pivot">
-                <div className="pivot-wrapper">
-                    <h1>Sign Up Today</h1>
-                    <div className="button-div">
-                        <button className="tertiary-btn">
-                            <Link to={""}>Learn More</Link>
-                        </button>
-                        <button className="primary-btn">
-                            <Link to={"/auth/signup"}>Get Started</Link>
-                        </button>
-                    </div>
-                </div>
-            </section>
-            <Footer />
+  return (
+    <div className="landing-page">
+      <LandingNavBar />
+      <section className="landing-hero" id="landing-hero">
+        <div className="hero-content">
+          <div className="hero-tagline">
+            <h1>Drive X</h1>
+            <h2>Securely upload and access your files from anywhere.</h2>
+            <div className="button-div">
+              <Link to="/auth/signup">
+                <button className="primary-btn">Get Started</button>
+              </Link>
+              <button className="tertiary-btn">Learn More</button>
+            </div>
+          </div>
         </div>
-    );
-}
+        <img src="" alt="drive-x" />
+      </section>
+      <section className="landing-features" id="landing-features">
+        <div className="feature">
+          <img src="" alt="encryption" />
+          <div className="feature-description">
+            <h1>End to End Encryption</h1>
+            <button className="tertiary-btn">Read More</button>
+          </div>
+        </div>
+        <div className="feature">
+          <img src="" alt="uploads" />
+          <div className="feature-description">
+            <h1>Fast Uploads</h1>
+            <button className="tertiary-btn">Read More</button>
+          </div>
+        </div>
+        <div className="feature">
+          <img src="" alt="folders" />
+          <div className="feature-description">
+            <h1>Smart Folders</h1>
+            <button className="tertiary-btn">Read More</button>
+          </div>
+        </div>
+      </section>
+      <section className="landing-pivot" id="landing-pivot">
+        <div className="pivot-wrapper">
+          <h1>Sign Up Today</h1>
+          <div className="button-div">
+            <button className="tertiary-btn">
+              <Link to={""}>Learn More</Link>
+            </button>
+            <Link to={"/auth/signup"}>
+              <button className="primary-btn">Get Started</button>
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </div>
+  );
+};
 
 export default Landing;
