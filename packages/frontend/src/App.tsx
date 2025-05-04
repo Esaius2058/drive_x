20023ff460c1135a860481c12331b3927eae9284
Esaius2058 <wabwirewild@gmail.com>
2025-05-04 21:18:30 +0300

    created a div in the AuthProvider element

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import {Outlet} from "react-router-dom"
-import './App.css'
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import { AppProps } from "next/app";
+import { AuthProvider } from "./components/AuthProvider";
 
 function App() {
   return (
-    <div className="app-container">
-      <Outlet />
-    </div>
-  )
+    <AuthProvider>
+      <div className="app-container">
+        <Outlet />
+      </div>
+    </AuthProvider>
+  );
 }
 
 export default App;
