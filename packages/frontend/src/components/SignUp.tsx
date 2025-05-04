a1570c238be6e54827b6066bca8d4a83c7ab0f05
Esaius2058 <wabwirewild@gmail.com>
2025-05-04 21:26:04 +0300

    formatted the file using prettier

--- packages/frontend/src/components/SignUp.tsx
+++ packages/frontend/src/components/SignUp.tsx
@@ -1,35 +1,121 @@
 import { Link } from "react-router-dom";
+import {useRef, useState } from "react";
+import { useAuth } from "./AuthContext";
+import Dashboard from "./Dashboard";
 
 const SignUp = () => {
-    return (
+  const [authType, setAuthType] = useState("sign-up");
+  const [authHeader, setAuthHeader] = useState("Sign Up");
+  const [authIntro, setAuthIntro] = useState("Already");
+  const [authCTA, setAuthCTA] = useState("Log In");
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [error, setError] = useState("");
+
+  const { signup, login, user, userFiles} = useAuth();
+  const signupForm = useRef<HTMLFormElement>(null);
+  const loginForm = useRef<HTMLFormElement>(null);
+  const handleAuthTypeChange = () => {
+    if (authType === "sign-up") {
+      setAuthType("log-in");
+      setAuthHeader("Log In");
+      setAuthIntro("Don't");
+      setAuthCTA("Sign Up");
+    } else {
+      setAuthType("sign-up");
+      setAuthHeader("Sign Up");
+      setAuthIntro("Already");
+      setAuthCTA("Log in");
+    }
+  };
+
+  const handleSignup = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const form = signupForm.current;
+    if (!form) return;
+
+    const formData = new FormData(form);
+
+    const firstName = formData.get("firstname") as string;
+    const lastName = formData.get("lastname") as string;
+
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    await signup(firstName, lastName, email, password);
+
+    setIsAuthenticated(true);
+  };
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const form = loginForm.current;
+    if (!form) {
+      console.warn("loginForm ref is null");
+      return;
+    }
+
+    const formData = new FormData(form);
+
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    await login(email, password);
+
+    setIsAuthenticated(true);
+  };
+
+  return (
+    <>
+      {isAuthenticated ? (
+        <Dashboard userData={userFiles}/>
+      ) : (
         <div className="auth-page">
-            <div className="auth-form">
-                <div className="auth-div"><img src="/xmark-solid.svg" alt="cancel" /></div>
-                <div className="auth-div-1"><h1>Sign Up</h1></div>
-                <div className="auth-div"><p>Already have an account? <Link to="/auth/login">Log In</Link></p></div>
-                <form action="/api/sign-up" method="POST">
-                    <input type="text" placeholder="first name" required />
-                    <input type="text" placeholder="last name" required />
-                    <input type="email" placeholder="email" required />
-                    <input type="password" placeholder="password" required />
-                    <button type="submit">Sign Up</button>
-                </form>
+          <div className="auth-form">
+            <div className="auth-div">
+              <Link to={"/"} className="cancel-btn">
+                <img src="/xmark-solid.svg" alt="cancel" />
+              </Link>
             </div>
-            <div className="auth-form">
-            <div className="auth-div"><img src="/xmark-solid.svg" alt="cancel" /></div>
-                <div className="auth-div-1"><h1>Log In</h1></div>
-                <div className="auth-div"><p>Don't have an account? <Link to="/auth/signup">Sign Up</Link></p></div>
-                <form action="/api/log-in" method="POST">
-                    <input type="email" placeholder="email" required />
-                    <input type="password" placeholder="password" required />
-                    <div className="button-div">
-                        <button className="secondary-btn">Forgot Password?</button>
-                        <button className="primary-btn" type="submit">Log In</button>
-                    </div>
-                </form>
+            <div className="auth-div-1">
+              <h1>{authHeader}</h1>
             </div>
+            <div className="auth-div">
+              <p>
+                {authIntro} have an account?{" "}
+                <Link to={""} onClick={handleAuthTypeChange}>
+                  {authCTA}
+                </Link>
+              </p>
+            </div>
+            {authType === "sign-up" ? (
+              <form id="signup-form" ref={signupForm} onSubmit={handleSignup}>
+                <input name="firstname" type="text" placeholder="first name" required />
+                <input name="lastname" type="text" placeholder="last name" required />
+                <input name="email" type="email" placeholder="email" required />
+                <input name="password" type="password" placeholder="password" required />
+                <button type="submit" className="primary-btn">
+                  {authHeader}
+                </button>
+              </form>
+            ) : (
+              <form id="login-form" ref={loginForm} onSubmit={handleLogin}>
+                <input name="email" type="email" placeholder="email" required />
+                <input name="password" type="password" placeholder="password" required />
+                <div className="button-div">
+                  <button className="secondary-btn">Forgot Password?</button>
+                  <button type="submit" className="primary-btn">
+                    Log In
+                  </button>
+                </div>
+              </form>
+            )}
+          </div>
         </div>
-    );
-}
+      )}
+    </>
+  );
+};
 
 export default SignUp;
