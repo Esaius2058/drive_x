84b13e92da8b98867e00591e24621e14e3a15a39
Esaius2058 <wabwirewild@gmail.com>
2025-05-06 10:08:10 +0300

    login and signup functions to post userdata to the backend
    store the token in localStorage
    
    each function returns the user in the request

---
+++ packages/frontend/src/services/auth.ts
@@ -0,0 +1,44 @@
+export async function loginUser(email: string, password: string) {
+  const res = await fetch("http://localhost:3000/api/log-in", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok)
+    throw new Error(data.message || "Internal Server Error. Login failed");
+
+  // Save the token
+  localStorage.setItem("token", data.token);
+
+  return data.user;
+}
+
+export async function signUpUser(
+  firstname: string,
+  lastname: string,
+  email: string,
+  password: string
+) {
+  const res = await fetch("http://localhost:3000/api/sign-up", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ firstname, lastname, email, password }),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok)
+    throw new Error(data.message || "Internal Server Error. Sign up failed");
+
+  // Save the token
+  localStorage.setItem("token", data.token);
+
+  return data.user;
+}
