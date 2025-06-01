128b9399c5519ba3a16fd27c4e91268f592fd792
Esaius2058 <wabwirewild@gmail.com>
2025-06-01 11:25:19 +0300

    feat: wrapped routes in ErrorBoundary

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,18 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider} from 'react-router-dom'
-import MainRoutes from './routes'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainRoutes from "./routes";
+import { ErrorBoundary } from "./components/ErrorBoundary";
+import "./index.css";
 
 const router = createBrowserRouter(MainRoutes());
-const rootElement = document.getElementById('root') as HTMLElement;
+const rootElement = document.getElementById("root") as HTMLElement;
 
 if (!rootElement) {
-  throw new Error('Root element not found')
+  throw new Error("Root element not found");
 }
 
 createRoot(rootElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+    <ErrorBoundary>
+      <RouterProvider router={router} />
+    </ErrorBoundary>
+  </StrictMode>
 );
