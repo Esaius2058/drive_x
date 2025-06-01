9bc181e252afe7e6ff1296f2ea009eb3dc14e98f
Esaius2058 <wabwirewild@gmail.com>
2025-06-01 11:10:24 +0300

    feat(compoent): created ErrorFallback to manage error handling
    fix(hook): added useNavigate to navigate back to dashboard

--- packages/frontend/src/components/404.tsx
+++ packages/frontend/src/components/404.tsx
@@ -1,47 +1,60 @@
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
+  const navigate = useNavigate();
+  const handleRedirect = () => {
+    navigate("/dashboard");
+  };
   return (
     <div className="error-page">
       <h1>404</h1>
       <h2>The page you're looking for doesn't exist.</h2>
-      <button className="primary-btn">Back to Dashboard</button>
+      <button className="primary-btn" onClick={() => handleRedirect()}>
+        Back to Dashboard
+      </button>
     </div>
   );
 };
 
 const NoFiles = () => {
-    return (
-        <div className="no-section">
-        <h1>No files here</h1>
-        <h2>It seems you don't have any files yet.</h2>
-        <button className="primary-btn">Upload Files</button>
-        </div>
-    );
-}
+  return (
+    <div className="no-section">
+      <h1>No files here</h1>
+      <h2>It seems you don't have any files yet.</h2>
+      <button className="primary-btn">Upload Files</button>
+    </div>
+  );
+};
 
 const EmptyTrash = () => {
-    return(
-        <div className="no-section">
-            <h1>Trash is empty</h1>
-            <h2>Looks like you haven't deleted anything yet.</h2>
-        </div>
-    );
-}
+  return (
+    <div className="no-section">
+      <h1>Trash is empty</h1>
+      <h2>Looks like you haven't deleted anything yet.</h2>
+    </div>
+  );
+};
 
 const NoStarred = () => {
-    return(
-        <div className="no-section">
-            <h1>No starred files</h1>
-            <h2>Looks like you haven't starred any files yet.</h2>
-        </div>
-    );
-}
+  return (
+    <div className="no-section">
+      <h1>No starred files</h1>
+      <h2>Looks like you haven't starred any files yet.</h2>
+    </div>
+  );
+};
 
 const NoShared = () => {
-    return(
-        <div className="no-section">
-            <h1>No shared files</h1>
-            <h2>Looks like you haven't shared any files yet.</h2>
-        </div>
-    );
-}
-export {ErrorPage, NoFiles, EmptyTrash, NoStarred, NoShared};
+  return (
+    <div className="no-section">
+      <h1>No shared files</h1>
+      <h2>Looks like you haven't shared any files yet.</h2>
+    </div>
+  );
+};
+
+const ErrorFallBack = () => {
+  return <div>Something broke. Try refreshing.</div>;
+};
+
+export { ErrorPage, ErrorFallBack, NoFiles, EmptyTrash, NoStarred, NoShared };
