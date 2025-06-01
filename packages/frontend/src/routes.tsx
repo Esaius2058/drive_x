380c22b438032ed53784558c8c7fe0373db7ae23
Esaius2058 <wabwirewild@gmail.com>
2025-06-01 11:24:26 +0300

    feat: wrapped Dashboard in ErrorBoundary

--- packages/frontend/src/routes.tsx
+++ packages/frontend/src/routes.tsx
@@ -2,45 +2,56 @@ import App from "./App";
 import Landing from "./components/Landing";
 import AuthWrapper from "./components/AuthWrapper";
 import SignUp from "./components/SignUp";
-import {Dashboard} from "./components/Dashboard";
+import { Dashboard } from "./components/Dashboard";
 import AdminDashboard from "./components/AdminDashboard";
-import {ErrorPage} from "./components/404";
+import { ErrorPage } from "./components/404";
+import { ErrorBoundaryWrapper } from "./components/ErrorBoundary";
 
 const MainRoutes = () => {
-    const routes = [{
-        path: "/",
-        element: <App />,
-        children: [{
-            index: true,
-            element: <Landing />
+  const routes = [
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: <Landing />,
         },
         {
-            path: "auth",
-            element: <AuthWrapper />,
-            children: [{
-                path: "signup",
-                element: <SignUp />
+          path: "auth",
+          element: <AuthWrapper />,
+          children: [
+            {
+              path: "signup",
+              element: <SignUp />,
             },
             {
-                path: "login",
-                element: <SignUp />
-            }]
+              path: "login",
+              element: <SignUp />,
+            },
+          ],
         },
         {
-            path: "dashboard",
-            element: <Dashboard />,
-        },{
-            path: "admin-dashboard",
-            element: <AdminDashboard />
+          path: "dashboard",
+          element: (
+            <ErrorBoundaryWrapper>
+              <Dashboard />
+            </ErrorBoundaryWrapper>
+          ),
         },
         {
-            path: "*",
-            element: <ErrorPage />,
-    }]
-    }
-    ];
+          path: "admin-dashboard",
+          element: <AdminDashboard />,
+        },
+        {
+          path: "*",
+          element: <ErrorPage />,
+        },
+      ],
+    },
+  ];
 
-    return routes;
-}
+  return routes;
+};
 
 export default MainRoutes;
