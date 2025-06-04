c7aa42195575c702032105ec9562e43dbbe6abeb
Esaius2058 <wabwirewild@gmail.com>
2025-06-04 17:58:37 +0300

    fix: bind my port to all network interfaces using '0.0.0.0'

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -24,4 +24,4 @@ app.use("/api", router);
 app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", () => console.log(`App listening on port ${PORT}`));
