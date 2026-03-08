e637360399dc41c3f0dfeb3f5496177addc66553
Esaius2058 <wabwirewild@gmail.com>
2026-03-08 20:32:20 +0300

    refactor: update frontend access to the backend api

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -13,7 +13,7 @@ app.set("views", path.resolve(__dirname, "../src", "views"));
 const allowedOrigins =
   process.env.NODE_ENV === "production"
     ? ["https://drive-x-jiuy.onrender.com", "https://file-uploader-nu.vercel.app"]
-    : ["http://localhost:5173", "https://drive-x-jiuy.onrender.com"];
+    : ["http://localhost:5173", "http://13.220.4.43/"];
 
 app.use(
   cors({
